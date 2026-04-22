ebd48f4c33b6818e12659e56f950a9dc917952c9
trystbj <trystbj@users.noreply.github.com>
2026-04-22 20:34:12 +0900

    chore(web): remove desktop files and slim repo

--- backend/routes/storyRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { generateKatha } from '../controllers/storyController.js'
-
-export const storyRoutes = Router()
-
-storyRoutes.post('/generate-katha', generateKatha)
-
